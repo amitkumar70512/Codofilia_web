208349ebaa07be1e4c2f67c5eec15c5c2d664f55
amitkumar.cs19@bmsce.ac.in <amitkumar.cs19@bmsce.ac.in>
2022-01-26 14:19:25 +0530

    heroku pushing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var firebase = require("firebase-admin");
+var admin = require("firebase-admin");
 const functions = require('firebase-functions');
 var serviceAccount = require("./serviceAccountKey.json");
- 
+require("dotenv").config();
  ////
 
 const express = require('express');  
@@ -27,154 +27,25 @@ app.use(bodyParser.urlencoded({
 	extended:true
 }));
 
-
-
 app.use(express.static('public'));
-  
- app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 
 
-
-
-
-  
 
 // To Run the server with Port Number  
 app.listen(port,()=> console.log(`Express server is running at port no :${port}`));  
   
-// CRUD Methods  
-
-  
-
-//      for registration table
 
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
  
-
-app.get("/index", function(req,res){
-    res.render('pages/index')
+app.get("/:id", function(req,res){
+    res.render(`pages/${req.params.id}`)
 });
-
-app.get("/register", function(req,res){
-    res.render('pages/register')
-});
-app.get("/about", function(req,res){
-    res.render('pages/about')
-});
-app.get("/services", function(req,res){
-    res.render('pages/services')
-});
-app.get("/login", function(req,res){
-    res.render('pages/login')
-});
-app.get("/team",function(req,res){
-    res.render('pages/team')
-})
-app.get("/ebook",function(req,res){
-    res.render('pages/ebook')
-})
-app.get("/programs",function(req,res){
-    res.render('pages/programs')
-})
-app.get("/program",function(req,res){
-    res.render('pages/programs')
-})
-app.get("/program1",function(req,res){
-    res.render('pages/program1')
-})
-app.get("/program2",function(req,res){
-    res.render('pages/program2')
-})
-app.get("/program3",function(req,res){
-    res.render('pages/program3')
-})
-app.get("/program4",function(req,res){
-    res.render('pages/program4')
-})
-app.get("/program5",function(req,res){
-    res.render('pages/program5')
-})
-
-app.get("/program6",function(req,res){
-    res.render('pages/program6')
-})
-app.get("/program7",function(req,res){
-    res.render('pages/program7')
-})
-app.get("/program8",function(req,res){
-    res.render('pages/program8')
-})
-app.get("/program9",function(req,res){
-    res.render('pages/program9')
-})
-app.get("/program10",function(req,res){
-    res.render('pages/program10')
-})
-app.get("/program11",function(req,res){
-    res.render('pages/program11')
-})
-app.get("/program12",function(req,res){
-    res.render('pages/program12')
-})
-app.get("/program13",function(req,res){
-    res.render('pages/program13')
-})
-app.get("/install1",function(req,res){
-    res.render('pages/install1')
-})
-app.get("/install2",function(req,res){
-    res.render('pages/install2')
-})
-
-app.get("/contact",function(req,res){
-    res.render('pages/contact')
-})
-app.get("/8051material1",function(req,res){
-    res.render('pages/8051material1')
-})
-app.get("/8051material2",function(req,res){
-    res.render('pages/8051material2')
-})
-app.get("/8051material3",function(req,res){
-    res.render('pages/8051material3')
-})
-app.get("/8051material4",function(req,res){
-    res.render('pages/8051material4')
-})
-app.get("/8051material5",function(req,res){
-    res.render('pages/8051material5')
-})
-
-app.get("/firstpage",function(req,res){
-    res.render('pages/firstpage')
-})
-
-// app.get("/program/:id",function(){
-//     var path=pages/program
-//     res.render('pages/program'+req.params.id)
-// })
-
-app.get("/compiler",function(req,res)
-{
-    res.render('pages/compiler')
-})
-
-
-
-
-
-
-
-
-// working fine
-
-
 
 // for foooter newsletter
-
 app.post('/subscribe',[
     check('email','please enter email properly')
        .exists()
@@ -195,14 +66,55 @@ app.post('/subscribe',[
         })
 
     }
-
-    
-    
-    
-    
     
 });
 
+
+app.post('/submit',[
+    check('name', 'Please enter valid username without space..')
+        .exists()
+        .isLength({ min: 3 })
+        .isAlpha(),
+    check('email', 'Please provide valid email')
+        .isEmail()
+        
+        .normalizeEmail(),
+           
+    check(
+            "passkey",
+            " password must have  at least one  number and minumum length 5. ",
+          )
+      .isLength({ min: 5 })
+      .matches(
+        
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,20}$/,
+              ),
+ check('confirmpasskey', 'Passwords do not match').custom((value, {req}) => (value === req.body.passkey)),
+            
+
+],async(req,res)=>{
+name=req.body.name;
+email=req.body.email;
+
+pass1=req.body.passkey;
+pass2=req.body.confirmpasskey;
+console.log(name+email+""+pass1+pass2)
+const encryptkey = await bcrypt.hash(req.body.passkey, saltRounds)
+const writeResult = await admin.firestore().collection('user').doc(email).set({
+    name: name,
+    password: encryptkey
+    })
+    .then(function() {console.log("Document successfully written!");
+    var alert={msg:`Hurray!! '${name}' registered successfully`};
+    res.render('pages/register',{
+        alert:alert.msg
+    })
+    })
+    .catch(function(error) {console.error("Error writing document: ", error);});
+
+
+
+})
 
 
 
@@ -213,15 +125,15 @@ app.post('/subscribe',[
 ///////
 
 
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
   
 });
 
-var db = firebase.firestore();
+var db = admin.firestore();
 
 async function getFirestore(){
-    const firestore_con  = await firebase.firestore();
+    const firestore_con  = await admin.firestore();
     const writeResult = firestore_con.collection('sample').doc('sample_doc').get().then(doc => {
     if (!doc.exists) { console.log('No such document!'); }
     else {return doc.data();}})
@@ -243,7 +155,7 @@ async function getFirestore(){
 
 
         async function insertFormData(request){
-            const writeResult = await firebase.firestore().collection('sample').add({
+            const writeResult = await admin.firestore().collection('sample').add({
             name: request.body.email,
             password: request.body.passkey
             })
