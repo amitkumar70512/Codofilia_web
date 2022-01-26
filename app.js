4f261e48707d457759777aab68dc88c452955a2d
amitkumar.cs19@bmsce.ac.in <amitkumar.cs19@bmsce.ac.in>
2022-01-26 19:49:08 +0530

    push

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var admin = require("firebase-admin");
 const functions = require('firebase-functions');
 var serviceAccount = require("./serviceAccountKey.json");
-require("dotenv").config();
- ////
-
+//////////////////////////////////////////
 const express = require('express');  
 const port = process.env.PORT || 4000;
 const path=require('path');
@@ -15,14 +13,16 @@ const ejs = require('ejs');
 
 // for encryption
 const bcrypt = require('bcrypt');
-const { firestore } = require("firebase-admin");
 const jwt= require("jsonwebtoken");
-const { AuthRegistrationsCredentialListMappingContext } = require("twilio/lib/rest/api/v2010/account/sip/domain/authTypes/authRegistrationsMapping/authRegistrationsCredentialListMapping");
+
+const { verify } = require("crypto");
 const saltRounds = 10;
 
 var app = express();  
-
-    
+const dotenv = require('dotenv');
+// get config vars
+dotenv.config();
+process.env.TOKEN_SECRET;
 
 
 app.use(bodyParser.urlencoded({
@@ -42,10 +42,45 @@ app.listen(port,()=> console.log(`Express server is running at port no :${port}`
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
- 
-app.get("/:id", function(req,res){
-    res.render(`pages/${req.params.id}`)
+app.get('/register', function (req, res) {
+    res.render('pages/register');
 });
+app.get('/login', function (req, res) {
+    res.render('pages/login');
+});
+app.get("/:id",authenticateToken, function(req,res){
+   
+   res.render(`pages/${req.params.id}`)
+    
+});
+//
+function generateAccessToken(username) {
+    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+  }
+
+  function authenticateToken(req, res, next) {
+      
+    const authHeader = req.headers.cookie;
+    const token = authHeader && authHeader.split(' ')[1]
+    const finaltoken=token && token.split('=')[1]
+    if (token == null) {
+        res.render('pages/login',{
+            alert:'Unauthorized access, Log in First'
+        })
+        
+        }
+        else{
+            jwt.verify(finaltoken, process.env.TOKEN_SECRET, (err, user) => {
+           
+        
+            if (err) return res.sendStatus(403)
+        
+            // req.user = user
+        
+            next()
+            })
+        }
+  }
 
 // for foooter newsletter
 app.post('/subscribe',[
@@ -71,9 +106,9 @@ app.post('/subscribe',[
     
 });
 
+var name=''
 
-
-app.post('/register',[
+app.post('/registration',[
     check('name', 'Please enter valid username without space..')
         .exists()
         .isLength({ min: 3 })
@@ -104,13 +139,7 @@ pass2=req.body.confirmpasskey;
 console.log(name+email+""+pass1+pass2)
 const encryptkey = await bcrypt.hash(req.body.passkey, saltRounds)
 
-const user=new AuthRegistrationsCredentialListMappingContext({
-    name:req.body.name,
-    email:req.body.email,
-    password:pass1,
-    confirmpassword:pass2
-})
-const token= await  user.generateAuthToken()
+
 
 const writeResult = await admin.firestore().collection('user').doc(email).set({
     name: name,
@@ -123,18 +152,83 @@ const writeResult = await admin.firestore().collection('user').doc(email).set({
     })
     })
     .catch(function(error) {console.error("Error writing document: ", error);});
-
-
-
 })
 
 
-
-
-
-
-
 ///////
+app.post('/logging', function(req,res,next){
+    const email=String(req.body.email)
+    const password=String(req.body.passkey)
+    const firestore_con  =  admin.firestore();
+    const writeResult = firestore_con.collection('user').doc(req.body.email).get()
+    .then(doc => {
+        if (!doc.exists) // entered email user doesnt registered
+        { 
+            console.log('No such document!');
+           
+            res.render('pages/login', {
+                alert:' Invalid Crudentials..'
+            })
+
+        }
+        else { // block for password matching and others
+             
+              db_pass=doc.data().password;
+              
+             bcrypt.compare(password, db_pass, function(err, result) {
+                if (!result) 
+                {// password mismatch
+
+                    console.log('password not matched')
+                    res.render('pages/login', {
+                          alert:'crudentials not matched..'
+                      })
+   
+                } // end of password mismatch
+                else
+                { // password matched 
+                   
+                    console.log("password matched");
+                    const user={
+                        id:'amit',
+                        username:name,
+                        email:email
+                    }
+                     const token = generateAccessToken(user);
+                     console.log("token is created")
+                     res.cookie("jwt_authentication",token,{
+                         expires: new Date(Date.now() + 10000),
+                         httpOnly:true
+                     }); 
+                    
+                    res.render('pages/login',{
+                        alert:`${name} logged in succesfully!!`
+                    })
+                    
+                           ////////////////////////     
+                }// end of password matched
+
+            });
+
+        }// end of else block for password matching 
+        
+    }) // end of then
+
+    .catch(err => {
+         console.log('Error getting document', err);
+         const errors=[
+            {msg:'Failed! '+err}
+        ]
+        const alert = errors
+        res.render('pages/login', {
+               alert
+           })
+     }); // end of catch
+    
+});
+
+
+
 
 
 admin.initializeApp({
@@ -176,7 +270,7 @@ async function getFirestore(){
             }
 
 
-        app.post('/login',async (request,response) =>{
+        app.post('/',async (request,response) =>{
             var insert = await insertFormData(request);
             var alert = await getFirestore();
             console.log(alert)
